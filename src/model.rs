b9cfe746ba4c67f6acd3922e9eb227ada0aa1c9b
Alpha <alpd2110@gmail.com>
2020-03-09 13:51:08 -0400

    id to integration-test-model-dump
    Co-Authored-By: Korede Aderele <kaderele@gmail.com>

--- src/model.rs
+++ src/model.rs
@@ -1140,7 +1140,7 @@ impl Component for Model {
                 { view_menu_bar(&self) }
 
                 { view_tab_bar(&self) }
-                <input id="model" hidden=true >{serialized_model}</input>
+                <input id="integration-test-model-dump" hidden=true >{serialized_model}</input>
 
                 <div class="main">
                     <div id="grammars" class="grid-wrapper" style={zoom}
